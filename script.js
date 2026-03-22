603868bb4249e3bf752964d855921c6a1f76c93c
M.MAGESHBOOPATHI <mageshboopathi1352@gmail.com>
2026-03-22 21:43:59 +0530

    Refactor cart functionality and add item display
    Refactored addToCart function to accept parameters and added updateCart function to display cart items.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function addToCart() {
-  alert("Added to cart!");
+let cart = [];
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  updateCart();
+}
+
+function updateCart() {
+  let cartBox = document.getElementById("cart");
+  cartBox.innerHTML = "";
+
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item.name + " - ₹" + item.price;
+    cartBox.appendChild(li);
+  });
 }
